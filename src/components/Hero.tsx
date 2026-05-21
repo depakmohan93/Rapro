19668ea25a7326ef1f5c4401cbd964f6205528bb
Deepak <depakmohan93@gmail.com>
2026-05-21 15:17:04 +0000

    Fix: correct GridApps iframe domain to mathiverse.gridapps.in

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -170,7 +170,7 @@ export default function Hero() {
             <div className="absolute -top-20 -left-4 lg:-left-20 z-20 pointer-events-none"><FloatingBadge /></div>
             <div id="consultation-form" className="w-full">
               <iframe
-                src="https://rajam-property.gridapps.in/f/68ea0d1e-9210-4036-81c1-a2ff3d713767?embed=1&utm_source=mathiverse&utm_medium=form&utm_campaign=lead-form-chennai&utm_content=rajam-property"
+                src="https://mathiverse.gridapps.in/f/68ea0d1e-9210-4036-81c1-a2ff3d713767?embed=1&utm_source=mathiverse&utm_medium=form&utm_campaign=lead-form-chennai&utm_content=rajam-property"
                 width="100%"
                 height="600"
                 frameBorder="0"
